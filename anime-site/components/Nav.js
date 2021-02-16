ade4ca94ee93cdb86892643687c1e163096a16e5
Sydney <sydtaylo@gmail.com>
2021-02-15 23:44:51 -0800

    style header and navigation

--- anime-site/components/Nav.js
+++ anime-site/components/Nav.js
@@ -7,27 +7,24 @@ import {
   Text,
   Box,
   Heading,
+  Center
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Image } from "@chakra-ui/react"
 import NextLink from "next/link";
 
 const Nav = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <nav>
-      <Flex m="2" p="2">
-        <Box>
-          <Heading size="md" mr="4">
-            AnimeDB
-          </Heading>
+      <Flex m="2" p="2" layerStyle={colorMode === "light" ? "header_light" : "header_dark"}>
+        <Box mr="4">
+          <Image boxSize="40px" src="/coffee_jelly3.svg" alt="Coffee Jelly Logo" />
         </Box>
         <Box>
-          <NextLink href="/">
-            <Link mr="4">Home</Link>
-          </NextLink>
-          <NextLink href="/Anime">
-            <Link>Anime</Link>
-          </NextLink>
+          <Heading size="md" mr="4" textStyle="h2">
+            animé café
+          </Heading>
         </Box>
         <Spacer />
         <Box>
@@ -36,6 +33,16 @@ const Nav = () => {
             icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
           />
         </Box>
+      </Flex>
+      <Flex>
+        <Center layerStyle={colorMode === "light" ? "navbar_light" : "navbar_dark"}>
+          <NextLink href="/">
+            <Link mr="4">Home</Link>
+          </NextLink>
+          <NextLink href="/Anime">
+            <Link>Animé</Link>
+          </NextLink>
+        </Center>
       </Flex>
     </nav>
   );
