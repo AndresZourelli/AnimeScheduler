801b259398fd342c9594bc7910ff8bf223864dd2
Andres Zourelli <azourelli@gmail.com>
2021-03-21 01:20:21 -0700

    updated scraping

--- anime-scrapper/animeScraper/animeScraper/items.py
+++ anime-scrapper/animeScraper/animeScraper/items.py
@@ -4,8 +4,6 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from w3lib.html import remove_tags
-from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
 
 def fixName(value):
@@ -14,28 +12,49 @@ def fixName(value):
         return first_name+" "+last_name
     return value
 
-
+def fixImage(value):
+    fixedSting = value.replace("/r/42x62","").split("?s=", 1)[0]
+    return fixedSting
 
 class ActorItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field( input_processor=MapCompose(fixName), output_processor = TakeFirst())
-    image_url = scrapy.Field( output_processor = TakeFirst())
+    image_url = scrapy.Field( input_processor=MapCompose(fixImage), output_processor = TakeFirst())
     actor_language = scrapy.Field( output_processor = TakeFirst())
     anime = scrapy.Field( output_processor = TakeFirst())
     character = scrapy.Field(input_processor=MapCompose(fixName), output_processor = TakeFirst())
+    character_image_url = scrapy.Field(input_processor=MapCompose(fixImage), output_processor = TakeFirst())
 
 class StaffItem(scrapy.Item):
     name = scrapy.Field( input_processor=MapCompose(fixName), output_processor = TakeFirst())
-    image_url = scrapy.Field( output_processor = TakeFirst())
+    image_url = scrapy.Field(input_processor=MapCompose(fixImage), output_processor = TakeFirst())
     role = scrapy.Field( output_processor = TakeFirst())
     anime = scrapy.Field( output_processor = TakeFirst())
 
 class CharacterItem(scrapy.Item):
     name = scrapy.Field( input_processor=MapCompose(fixName), output_processor = TakeFirst())
-    image_url = scrapy.Field( output_processor = TakeFirst())
+    image_url = scrapy.Field( input_processor=MapCompose(fixImage), output_processor = TakeFirst())
     role = scrapy.Field( output_processor = TakeFirst())
     anime = scrapy.Field( output_processor = TakeFirst())
-    actor = scrapy.Field(input_processor=MapCompose(fixName), output_processor = TakeFirst())
 
 class AnimeItem(scrapy.Item):
     title = scrapy.Field(output_processor = TakeFirst())
+    aired_start = scrapy.Field(output_processor = TakeFirst())
+    aired_end = scrapy.Field(output_processor = TakeFirst())
+    alt_names = scrapy.Field(output_processor = TakeFirst())
+    broadcast_day = scrapy.Field(output_processor = TakeFirst())
+    broadcast_time = scrapy.Field(output_processor = TakeFirst())
+    description = scrapy.Field(output_processor = TakeFirst())
+    duration = scrapy.Field(output_processor = TakeFirst())
+    episodes = scrapy.Field(output_processor = TakeFirst())
+    genres = scrapy.Field(output_processor = TakeFirst())
+    image_url = scrapy.Field(output_processor = TakeFirst())
+    licensors = scrapy.Field(output_processor = TakeFirst())
+    producers = scrapy.Field(output_processor = TakeFirst())
+    rating = scrapy.Field(output_processor = TakeFirst())
+    season = scrapy.Field(output_processor = TakeFirst())
+    source = scrapy.Field(output_processor = TakeFirst())
+    status = scrapy.Field(output_processor = TakeFirst())
+    studios = scrapy.Field(output_processor = TakeFirst())
+    type = scrapy.Field(output_processor = TakeFirst())
+    avg_score = scrapy.Field(output_processor = TakeFirst())
